d40fe37639e2b2a0b2c8b067fede524c9a613076
peterkush <pkungu004@gmail.com>
2020-07-13 02:12:22 +0300

    added new bbc.html folder and modifired it

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def  index():
     newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+    topheadlines = newsapi.get_top_headlines(sources="")
 
 
     articles = topheadlines['articles']
