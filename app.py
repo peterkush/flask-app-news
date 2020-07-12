e961799ed10a49030d2092c12d8ffe963a3404f5
peterkush <pkungu004@gmail.com>
2020-07-13 02:34:15 +0300

    defined the bbc.html

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def  index():
     newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
-    topheadlines = newsapi.get_top_headlines(sources="")
+    topheadlines = newsapi.get_top_headlines(sources="al-jezeera-english")
 
 
     articles = topheadlines['articles']
@@ -33,6 +33,42 @@ def  index():
 
 
     return render_template('index.html', context=mylist)
+
+
+
+
+
+
+@app.route( '/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+ 
+
+ 
+    mylist = zip(news,desc,img)
+
+
+    return render_template('bbc.html', context=mylist)
+
+
+
 
 
 if __name__ == "__main__":
