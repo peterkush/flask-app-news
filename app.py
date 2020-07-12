30b824638550a14dddae7af6f9a8ac14c599f3cc
peterkush <pkungu004@gmail.com>
2020-07-13 00:54:07 +0300

    was  debugged and worked as expected

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask , render_template
 from newsapi import NewsApiClient
 
 
@@ -7,33 +7,33 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def  index():
     newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
 
 
-     articles = topheadlines['articles']
+    articles = topheadlines['articles']
 
-     desc = []
+    desc = []
+    news = []
+    img = []
 
-     news = []
-
-     img = []
-
-     for i in range(len(articles)):
-         myarticles = articles[i]
+    for i in range(len(articles)):
+        myarticles = articles[i]
 
 
-         news.append(myarticles['title'])
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
 
-         desc.append(myarticles['description'])
+ 
 
-         img.append(myarticles['urlToImage'])
-
-
-
-
-         mylist = zip(news,desc,img)
+ 
+    mylist = zip(news,desc,img)
 
 
-         return render_template('index.html',context=mylist)
+    return render_template('index.html', context=mylist)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
