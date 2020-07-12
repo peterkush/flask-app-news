d05a2d0bd21d20dcc23ea14f64929b61dbad2491
peterkush <pkungu004@gmail.com>
2020-07-12 23:37:48 +0300

    added virtual environment installed news api

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from newsapi import NewsApiClient
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key="3ecb0d19c3c24ba6a9ae749c20178705")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+
+
+     articles = topheadlines['articles']
+
+     desc = []
+
+     news = []
+
+     img = []
+
+     for i in range(len(articles)):
+         myarticles = 
