8e9f3a569f5f35e28781d40dcf81a8313b977ebf
peterkush <pkungu004@gmail.com>
2020-07-13 12:20:00 +0300

    defined the get news function and modified it

--- app/requests.py
+++ app/requests.py
@@ -10,4 +10,21 @@ api_key = app.config['NEWS_API_KEY']
 base_url  = app.config["NEWS_API_BASE_URL"]
 
 
+def get_news(source):
+    '''
+    Function that gets json responce to our url request
+    '''
+    get_article_url = base_url + api_key
+
+    with urllib.request.urlopen(get_article_url) as url:
+        get_article_data = url.read()
+        get_article_response = json.loads(get_article_data)
+
+        Article_results = None
+
+        if get_article_response['results']:
+            news_articles_list = get_article_response['results']
+            Article_results = process_results(Article_results_list)
+            return articles_results
+
 
